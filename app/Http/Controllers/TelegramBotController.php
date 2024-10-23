25f87a3a132a4e66dafa780b27bc6c62b0dbdb2b
Mohit SIngla <mhtkumar46@gmail.com>
2024-10-23 18:24:26 +0530

    added inline keyboards

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -10,23 +10,66 @@ class TelegramBotController extends Controller
     public function handleWebhook()
     {
         try {
-            $update = Telegram::getWebhookUpdates();
+            $updates = Telegram::getWebhookUpdates();
 
-            $chatId = $update['message']['chat']['id'];
-            // $text = $update['message']['text'];
+            if ($updates->has('message')) {
+                $message = $updates->getMessage();
+                $chatId = $message->getChat()->getId();
+                $text = $message->getText();
 
-            $response = "Baad mei aana! Mohit ji abhi muje develop kar rhe hai. \u{1F60A}";
+                // Call the function to send inline keyboard if user sends a specific command
+                if ($text == '/start') {
+                    $this->sendQuestionWithInlineKeyboard($chatId);
+                }
+            } elseif ($updates->has('callback_query')) {
+                // Handle the response from the inline keyboard
+                $callbackQuery = $updates->get('callback_query');
+                $data = $callbackQuery['data'];
+                $chatId = $callbackQuery['message']['chat']['id'];
 
-            Telegram::sendMessage([
-                'chat_id' => $chatId,
-                'text' => $response,
-                'parse_mode' => 'Markdown',
-            ]);
+                // You can now handle responses based on the $data (e.g., user choices)
+                if ($data == 'Pizza') {
+                    Telegram::sendMessage([
+                        'chat_id' => $chatId,
+                        'text' => 'You selected Pizza!'
+                    ]);
+                } else if($data == 'Burger') {
+                    Telegram::sendMessage([
+                        'chat_id' => $chatId,
+                        'text' => 'You selected Burger!'
+                    ]);
+                } else {
+                    Telegram::sendMessage([
+                        'chat_id' => $chatId,
+                        'text' => 'Invalid Option!'
+                    ]);
+                }
+            }
         } catch (\Exception $e) {
             Log::error($e->getMessage());
         }
 
         return response('OK', 200);
+    }
+
+    function sendQuestionWithInlineKeyboard($chatId)
+    {
+        // Define the inline keyboard
+        $replyMarkup = json_encode([
+            'inline_keyboard' => [
+                [
+                    ['text' => 'Pizza', 'callback_data' => 'Pizza'],
+                    ['text' => 'Burger', 'callback_data' => 'Burger']
+                ]
+            ]
+        ]);
+
+        // Send the message with the inline keyboard
+        Telegram::sendMessage([
+            'chat_id' => $chatId,
+            'text' => 'Please choose an option:',
+            'reply_markup' => $replyMarkup
+        ]);
     }
 
     public function setWebhook()
